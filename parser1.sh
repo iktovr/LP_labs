6efd7f04e5e669e8ac672661beffbb2147fb7ca6
iktovr <30842714+iktovr@users.noreply.github.com>
2020-09-24 15:58:43 +0300

    Add parser1 - bash version

---
+++ parser1.sh
@@ -0,0 +1,57 @@
+infile=$1
+outfile=$2
+
+> $outfile
+
+declare -A indis
+
+id=""
+IFS=$'\n'
+for tk in `grep -Eo '(0 @.*@ INDI|1 NAME .* ?/.*/)' $infile | sed -e 's/0 @/@/g; s/@ INDI/@/g; s/1 NAME *//g; y/\x27/\x22/; s/\///g; s/ *$//g'`; do
+	if [ "${tk:0:1}" = "@" ]; then
+		id=$tk
+	else
+		indis[$id]="\x27$tk\x27"
+	fi
+done
+
+i=0
+IFS=$' \n'
+for tk in `grep -Eo '(@.*@|HUSB|WIFE|CHIL) (FAM|@.*@)' $infile`; do
+	fams[$i]=$tk
+	i=$[ $i + 1 ]
+done
+
+i=0
+while [ $i -lt ${#fams[@]} ]; do
+	i=$[ $i + 2 ]
+	if [ ${fams[$i]} = HUSB ]; then
+		i=$[ $i + 1 ]
+		husb=${indis[${fams[$i]}]}
+	elif [ ${fams[$i]} = WIFE ]; then
+		i=$[ $i + 1 ]
+		wife=${indis[${fams[$i]}]}
+	else
+		continue
+	fi
+
+	i=$[ $i + 1 ]
+	if [ ${fams[$i]} = HUSB ]; then
+		i=$[ $i + 1 ]
+		husb=${indis[${fams[$i]}]}
+	elif [ ${fams[$i]} = WIFE ]; then
+		i=$[ $i + 1 ]
+		wife=${indis[${fams[$i]}]}
+	else
+		continue
+	fi
+
+	i=$[ $i + 1 ]
+	while [[ $i -lt ${#fams[@]} && ${fams[$i]} = CHIL ]]; do
+		i=$[ $i + 1 ]
+		chil=${indis[${fams[$i]}]}
+		echo -e "father($husb,$chil).\nmother($wife,$chil)." >> $outfile
+		i=$[ $i + 1 ]
+	done
+done
+sort -o $outfile $outfile
