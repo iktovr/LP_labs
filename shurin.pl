94d170252f990c352663153a9ae162f304e10d05
iktovr <30842714+iktovr@users.noreply.github.com>
2020-12-20 01:25:55 +0300

    Minor fix

--- shurin.pl
+++ shurin.pl
@@ -3,5 +3,5 @@
 sex(X,m) :- father(X,_).
 sex(X,f) :- mother(X,_).
 
-shurin(X,Y) :- sex(X,m), father(Y,A), mother(B,A), father(C,B), father(C,X).
-shurin(X,Y) :- sex(X,m), father(Y,A), mother(B,A), mother(C,B), mother(C,X).
+shurin(X,Y) :- father(Y,A), mother(B,A), father(C,B), father(C,X), sex(X,m).
+shurin(X,Y) :- father(Y,A), mother(B,A), mother(C,B), mother(C,X), sex(X,m).
